e90cdc56ba951dec94b413426a358a41c52b1cb6
Pratik Lahamge <89896925+Pratiklahamge@users.noreply.github.com>
2021-10-19 01:13:37 +0530

    UC2 valide last name
    valid Last Name - Last name starts with Cap and has
    minimum 3 characters

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -5,22 +5,20 @@ import java.util.Scanner;
  * Purpose  - As a User need to enter a valid First Name
 
  */
-
-public class UserRegistrationMain {
-
-	public static void welcome(){
-		System.out.println("Welcome to User Registration System Problem");
-	}
-	public static void main(String[] args) {
-		welcome();
-		welcome();
-		UserRegistration user = new UserRegistration();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter 1 : to validate First Name ");
-		switch (scanner.nextInt()){
-		case 1:
-			UserRegistration.validFirstName();
-			break;
-		}
-	}
+@@ -17,10 +17,16 @@ public static void main(String[] args) {
+    UserRegistration user = new UserRegistration();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter 1 : to validate First Name ");
+    System.out.println("Enter 2 : to validate Last Name ");
+    switch (scanner.nextInt()){
+        case 1:
+            UserRegistration.validFirstName();
+            break;
+        case 2:
+            UserRegistration.validLastName();
+            break;
+        default:
+            System.out.println("Select a valid number");
+    }
+}
 }
